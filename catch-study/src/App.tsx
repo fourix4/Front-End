fbc64cd6befefdeedf8d90afdb3b545ea3ec83f9
ejinn1 <ejinn1@naver.com>
2024-07-03 15:24:35 +0900

    [#8] feat : 채팅 route 설정

--- catch-study/src/App.tsx
+++ catch-study/src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import ChattingPage from './pages/ChattingPage/ChattingPage';
 import MainPage from './pages/MainPage/MainPage';
 
 const App: React.FC = () => {
   return (
     <>
-      <MainPage />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/chatting' element={<ChattingPage />} />
+      </Routes>
     </>
   );
 };
---
+++ catch-study/src/pages/ChattingPage/ChattingPage.tsx
@@ -0,0 +1,5 @@
+const ChattingPage: React.FC = () => {
+  return <div>채팅</div>;
+};
+
+export default ChattingPage;
