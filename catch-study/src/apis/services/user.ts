e27ef57528e6cbdc0f05680f44b296adcba5f6fd
HyunJungJo98 <kchwoonn0827@naver.com>
2024-07-05 15:14:58 +0900

    [#8] feat : request, response interceptor로 access token을 헤더에 설정, 만료 시 재요청 기능 구현

--- catch-study/src/apis/services/user.ts
+++ catch-study/src/apis/services/user.ts
@@ -2,6 +2,7 @@ import { LoginResponseTypes, ResponseTypes } from '../../types/interfaces';
 
 const isSuccessLogin = (rawPost: LoginResponseTypes | ResponseTypes) => {
   if (rawPost.code >= 200 && rawPost.code < 300) {
+    localStorage.setItem('accessToken', `${rawPost.data!.accessToken}`);
     return true;
   }
 
--- catch-study/src/apis/utils/axios.ts
+++ catch-study/src/apis/utils/axios.ts
@@ -1,11 +1,72 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { SERVER_URL } from '../../config/constants';
+import { LoginResponseTypes } from '../../types/interfaces';
 
 const instance = axios.create({
-  baseURL: `${SERVER_URL}/api`,
+  baseURL: `${SERVER_URL}api`,
   headers: {
     withCredentials: true,
   },
 });
+
+instance.interceptors.request.use(
+  config => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (accessToken && config.headers) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  },
+);
+
+const refreshAccessToken = async () => {
+  try {
+    const response = await instance.post<LoginResponseTypes>(
+      '/users/reissuance',
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+        withCredentials: true,
+      },
+    );
+
+    const { accessToken } = response.data.data;
+
+    localStorage.setItem('accessToken', accessToken);
+
+    return accessToken;
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
+  }
+};
+
+instance.interceptors.response.use(
+  response => response,
+  async (error: AxiosError) => {
+    if (!error.config || !error.response || error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+
+    try {
+      const newAccessToken = await refreshAccessToken();
+
+      error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+
+      return instance(error.config);
+    } catch (refreshError) {
+      console.log(refreshError);
+      return Promise.reject(refreshError);
+    }
+  },
+);
 
 export default instance;
