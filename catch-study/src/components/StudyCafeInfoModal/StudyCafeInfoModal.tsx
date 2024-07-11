35fb1985f79027988ee0e0300d216fcd40f53941
조현정 <79002373+HyunJungJo98@users.noreply.github.com>
2024-07-11 14:06:35 +0900

    [#14] style : 예약하기 버튼 생성

--- catch-study/src/components/StudyCafeInfoModal/StudyCafeInfoModal.tsx
+++ catch-study/src/components/StudyCafeInfoModal/StudyCafeInfoModal.tsx
@@ -55,47 +55,52 @@ const StudyCafeInfoModal: React.FC<StudyCafeInfoModalPropTypes> = ({
 
   return (
     <div
-      className={`fixed bottom-0 min-w-[300px] w-full p-30 rounded-t-default shadow-modal bg-white ${isOpen ? 'visible' : 'invisible'} duration-300 ease-out ${isOpen ? 'h-modal' : 'h-0'}`}
+      className={`fixed bottom-0 min-w-[300px] w-full rounded-t-default shadow-modal bg-white ${isOpen ? 'visible' : 'invisible'} duration-300 ease-out ${isOpen ? 'h-modal' : 'h-0'}`}
     >
-      <div className='relative'>
-        <div className='h-6 mb-20 w-50 rounded-default bg-light-gray m-middle'></div>
-        <button
-          onClick={closeModal}
-          className='absolute w-24 h-24 bg-center bg-no-repeat bg-close right-0 inset-y-1/2 translate-y-[-50%]'
-        ></button>
-      </div>
+      <div className='h-490 p-30'>
+        <div className='relative'>
+          <div className='h-6 mb-20 w-50 rounded-default bg-light-gray m-middle'></div>
+          <button
+            onClick={closeModal}
+            className='absolute w-24 h-24 bg-center bg-no-repeat bg-close right-0 inset-y-1/2 translate-y-[-50%]'
+          ></button>
+        </div>
 
-      {/* <SlideImage images={info.cafe_images} /> */}
-      <SlideImage images={[test1, test2, test3, test4]} />
+        {/* <SlideImage images={info.cafe_images} /> */}
+        <SlideImage images={[test1, test2, test3, test4]} />
 
-      <div className='mb-20'>
-        <p className='font-bold text-20 mb-15'>이용 가능 좌석</p>
-        <div>
-          <span className='mr-60'>
-            <span className='font-bold text-16 mr-25'>자유석</span>
-            <span className='text-16'>
-              {info.available_seats}/{info.total_seats}
+        <div className='mb-20'>
+          <p className='font-bold text-20 mb-15'>이용 가능 좌석</p>
+          <div>
+            <span className='mr-60'>
+              <span className='font-bold text-16 mr-25'>자유석</span>
+              <span className='text-16'>
+                {info.available_seats}/{info.total_seats}
+              </span>
             </span>
-          </span>
-          <span>
-            <span className='font-bold text-16 mr-25'>스터디룸</span>
-            <span className='text-16'>
-              {info.available_rooms}/{info.total_rooms}
+            <span>
+              <span className='font-bold text-16 mr-25'>스터디룸</span>
+              <span className='text-16'>
+                {info.available_rooms}/{info.total_rooms}
+              </span>
             </span>
+          </div>
+        </div>
+        <div className='mb-20'>
+          <img src={mapPin} className='inline mr-10' />
+          <span className='align-middle text-16'>{info.address}</span>
+        </div>
+        <div className='mb-20'>
+          <img src={time} className='inline mr-10' />
+          <span className='align-middle text-16'>
+            {info.opening_hours} ~ {info.closed_hours}
           </span>
         </div>
+        <div className='ml-30 text-16'>휴무일 {info.cloesd_day}</div>
       </div>
-      <div className='mb-20'>
-        <img src={mapPin} className='inline mr-10' />
-        <span className='align-middle text-16'>{info.address}</span>
-      </div>
-      <div className='mb-20'>
-        <img src={time} className='inline mr-10' />
-        <span className='align-middle text-16'>
-          {info.opening_hours} ~ {info.closed_hours}
-        </span>
-      </div>
-      <div className='ml-30 text-16'>휴무일 {info.cloesd_day}</div>
+      <button className={`w-full h-60 text-24 font-bold text-white bg-blue`}>
+        예약하기
+      </button>
     </div>
   );
 };
