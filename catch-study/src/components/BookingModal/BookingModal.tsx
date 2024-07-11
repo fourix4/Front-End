37b202baa83d27eb89c97706c4b054b9f3300784
조현정 <79002373+HyunJungJo98@users.noreply.github.com>
2024-07-11 22:46:35 +0900

    [#15] style : 스터디룸 선택 시 날짜, 시간 선택하는 모달창 노출

--- catch-study/src/components/BookingModal/BookingModal.tsx
+++ catch-study/src/components/BookingModal/BookingModal.tsx
@@ -29,15 +29,59 @@ const BookingModal: React.FC<BookingModalPropTypes> = ({
             날짜/시간 선택
           </div>
           <div className='flex w-full max-h-[85%] overflow-auto flex-wrap'>
-            {selectedSeat.type === SEAT_TYPE.SEAT
-              ? usageFee.map((fee, i) => (
-                  <div key={i} className='w-1/2 h-80 p-10'>
-                    <button className='w-full h-full border-[1px] border-dark-gray rounded-sm text-16'>
-                      {fee.hours}시간 {fee.price.toLocaleString()}원
-                    </button>
-                  </div>
-                ))
-              : ''}
+            {selectedSeat.type === SEAT_TYPE.SEAT ? (
+              usageFee.map((fee, i) => (
+                <div key={i} className='w-1/2 h-80 p-10'>
+                  <button className='w-full h-full border-[1px] border-dark-gray rounded-sm text-16'>
+                    {fee.hours}시간 {fee.price.toLocaleString()}원
+                  </button>
+                </div>
+              ))
+            ) : (
+              <div className='w-full max-h-[85%]'>
+                <div className='flex justify-between w-full h-60 mb-10'>
+                  <select className='px-10 w-1/3 h-60 mr-10 select cursor-pointer rounded-sm border-[1px] border-dark-gray text-16 text-center'>
+                    <option key={2024} className='text-16'>
+                      2024년
+                    </option>
+                  </select>
+
+                  <select
+                    defaultValue={new Date().getMonth() + 1}
+                    className='px-10 w-1/3 h-60 mr-10 select cursor-pointer rounded-sm border-[1px] border-dark-gray text-16 text-center'
+                  >
+                    {Array(12)
+                      .fill(0)
+                      .map((_, i) => (
+                        <option value={i + 1} className='text-16'>
+                          {i + 1}월
+                        </option>
+                      ))}
+                  </select>
+
+                  <select
+                    defaultValue={new Date().getDate()}
+                    className='px-10 w-1/3 h-60 mr-10 select cursor-pointer rounded-sm border-[1px] border-dark-gray text-16 text-center'
+                  >
+                    {Array(31)
+                      .fill(0)
+                      .map((_, i) => (
+                        <option value={i + 1} className='text-16'>
+                          {i + 1}일
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                <div className='flex justify-between w-full h-60'>
+                  <select className='px-10 w-1/2 h-60 mr-10 select cursor-pointer rounded-sm border-[1px] border-dark-gray text-16 text-center'>
+                    <option className='text-16'></option>
+                  </select>
+                  <select className='px-10 w-1/2 h-60 mr-10 select cursor-pointer rounded-sm border-[1px] border-dark-gray text-16 text-center'>
+                    <option className='text-16'></option>
+                  </select>
+                </div>
+              </div>
+            )}
           </div>
         </div>
         <button className={`w-full h-60 text-24 font-bold text-white bg-blue`}>
