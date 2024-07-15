8b16b9008a777c14d157f0c9ccd23c57814af871
ejinn1 <ejinn1@naver.com>
2024-07-15 11:33:42 +0900

    [#28] fix : 업장 조회 주석 수정, cafeInfo값 초기화 수정

--- catch-study/src/pages/ManagementPage/ManagementPage.tsx
+++ catch-study/src/pages/ManagementPage/ManagementPage.tsx
@@ -1,54 +1,26 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { getManagementInfo } from '../../apis/api/manager';
+import isExistCafeInfo from '../../apis/services/manager';
 import ManagementCafeInfo from '../../components/ManagementCafeInfo/ManagementCafeInfo';
 import Topbar from '../../components/Topbar/Topbar';
 import { ROUTE } from '../../config/constants';
 import { FormDataTypes } from '../../types/management';
 
 const ManagementPage: React.FC = () => {
-  const [cafeInfo, setCafeInfo] = useState<FormDataTypes>({
-    cafe_name: '이지 스터디',
-    address: {
-      city: '서울시',
-      country: '강남구',
-      town: '논현동',
-      etc: '123번지-1',
-    },
-    opening_hours: '10:00',
-    closed_hours: '20:00',
-    closed_day: '',
-    seats: 40,
-    room_info: [
-      {
-        cancel_available_time: 30,
-        rooms: [
-          { name: 'A', capacity: 2, price: 2000 },
-          { name: 'B', capacity: 4, price: 4000 },
-        ],
-      },
-    ],
-    usage_fee: [
-      { hours: 1, price: 2000 },
-      { hours: 2, price: 4000 },
-      { hours: 5, price: 5000 },
-    ],
-    title_image: '',
-    multiple_images: [],
-    seatChart_image: '',
-    cafe_phone: '',
-  });
+  const [cafeInfo, setCafeInfo] = useState<FormDataTypes>();
   const [isExist, setIsExist] = useState(true);
 
   useEffect(() => {
-    // (async () => {
-    //   const rawData = await getManagementInfo();
-    //   console.log(rawData);
-    //   if (isExistCafeInfo(rawData)) {
-    //     setIsExist(true);
-    //     return;
-    //   }
-    //   setCafeInfo(rawData.result as FormDataTypes);
-    // })();
+    (async () => {
+      const rawData = await getManagementInfo();
+      console.log(rawData);
+      if (isExistCafeInfo(rawData)) {
+        setIsExist(true);
+        return;
+      }
+      setCafeInfo(rawData.result as FormDataTypes);
+    })();
   }, [isExist, setIsExist, setCafeInfo]);
 
   return (
