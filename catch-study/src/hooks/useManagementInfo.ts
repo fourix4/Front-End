961d7cfdb81d965373df4e9be8de84d3c189bb4b
ejinn1 <ejinn1@naver.com>
2024-07-20 14:07:01 +0900

    [#60] refactor : 업장 정보 custom hook 리펙토링

--- catch-study/src/hooks/useManagementInfo.ts
+++ catch-study/src/hooks/useManagementInfo.ts
@@ -1,41 +1,25 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { useAtom } from 'jotai';
+import { ChangeEvent, useEffect } from 'react';
+
 import {
-  FormDataTypes,
-  RoomInfoTypes,
-  UsageFeeTypes,
-} from '../types/management';
+  cancelTimeAtom,
+  formDataAtom,
+  roomInfosAtom,
+  storeImagesAtom,
+  thumbnailAtom,
+  usageFeesAtom,
+} from '../atoms/management';
+import { RoomInfoTypes, UsageFeeTypes } from '../types/management';
+
+type ManagementChangeTypes = 'room' | 'usage';
 
 const useManagementInfo = () => {
-  const [roomInfos, setRoomInfos] = useState<RoomInfoTypes[]>([]);
-  const [cancelTime, setCancelTime] = useState(0);
-  const [usageFees, setUsageFees] = useState<UsageFeeTypes[]>([]);
-  const [thumbnail, setThumbnail] = useState<File | null>(null);
-  const [storeImages, setStoreImages] = useState<File[]>([]);
-
-  const [formData, setFormData] = useState<FormDataTypes>({
-    cafe_name: '',
-    address: {
-      city: '',
-      country: '',
-      town: '',
-      etc: '',
-    },
-    opening_hours: '',
-    closed_hours: '',
-    closed_day: '',
-    seats: 0,
-    room_info: [
-      {
-        cancel_available_time: cancelTime,
-        rooms: roomInfos,
-      },
-    ],
-    usage_fee: usageFees,
-    title_image: thumbnail,
-    multiple_images: storeImages,
-    seatChart_image: '',
-    cafe_phone: '',
-  });
+  const [roomInfos, setRoomInfos] = useAtom(roomInfosAtom);
+  const [cancelTime, setCancelTime] = useAtom(cancelTimeAtom);
+  const [usageFees, setUsageFees] = useAtom(usageFeesAtom);
+  const [thumbnail, setThumbnail] = useAtom(thumbnailAtom);
+  const [storeImages, setStoreImages] = useAtom(storeImagesAtom);
+  const [formData, setFormData] = useAtom(formDataAtom);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -61,36 +45,6 @@ const useManagementInfo = () => {
     }));
   };
 
-  const handleNestedInputChange = (
-    e: ChangeEvent<HTMLInputElement>,
-    field: string,
-    setData: React.Dispatch<React.SetStateAction<FormDataTypes>>,
-  ) => {
-    const { name, value } = e.target;
-
-    setData(prevFormData => ({
-      ...prevFormData,
-      [field]: {
-        ...prevFormData.address,
-        [name]: value,
-      },
-    }));
-  };
-
-  const handleRoomChange = (
-    index: number,
-    field: keyof RoomInfoTypes,
-    value: string,
-  ) => {
-    const numberValue = value === '' ? 0 : parseInt(value, 10);
-
-    setRoomInfos(prev =>
-      prev.map((room, i) =>
-        i === index ? { ...room, [field]: numberValue } : room,
-      ),
-    );
-  };
-
   const handleRoomNameChange = (
     e: ChangeEvent<HTMLInputElement>,
     index: number,
@@ -102,37 +56,46 @@ const useManagementInfo = () => {
     );
   };
 
-  const handleAddRoom = () => {
-    setRoomInfos(prev => [
-      ...prev,
-      { name: '', capacity: 0, counts: 0, price: 0 },
-    ]);
-  };
-
-  const handleRemoveRoom = (index: number) => {
-    setRoomInfos(prev => prev.filter((_, i) => i !== index));
-  };
-
-  const handleAddFee = () => {
-    setUsageFees(prev => [...prev, { hours: 0, price: 0 }]);
-  };
-
-  const handleRemoveFee = (index: number) => {
-    setUsageFees(prev => prev.filter((_, i) => i !== index));
-  };
-
-  const handleFeeChange = (
+  const handleArrayChange = (
     index: number,
-    field: keyof UsageFeeTypes,
+    filed: keyof RoomInfoTypes | keyof UsageFeeTypes,
     value: string,
+    type: ManagementChangeTypes,
   ) => {
     const numberValue = value === '' ? 0 : parseInt(value, 10);
 
-    setUsageFees(prev =>
-      prev.map((room, i) =>
-        i === index ? { ...room, [field]: numberValue } : room,
-      ),
-    );
+    if (type === 'room') {
+      setRoomInfos(prev =>
+        prev.map((item, i) =>
+          i === index ? { ...item, [filed]: numberValue } : item,
+        ),
+      );
+    } else {
+      setUsageFees(prev =>
+        prev.map((item, i) =>
+          i === index ? { ...item, [filed]: numberValue } : item,
+        ),
+      );
+    }
+  };
+
+  const handleAddItem = (type: ManagementChangeTypes) => {
+    if (type === 'room') {
+      setRoomInfos(prev => [
+        ...prev,
+        { name: '', capacity: 0, counts: 0, price: 0 },
+      ]);
+    } else {
+      setUsageFees(prev => [...prev, { hours: 0, price: 0 }]);
+    }
+  };
+
+  const handleRemoveItem = (index: number, type: ManagementChangeTypes) => {
+    if (type === 'room') {
+      setRoomInfos(prev => prev.filter((_, i) => i !== index));
+    } else {
+      setUsageFees(prev => prev.filter((_, i) => i !== index));
+    }
   };
 
   const handleThumbnailChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -157,34 +120,27 @@ const useManagementInfo = () => {
         },
       ],
       usage_fee: usageFees,
-      title_image: thumbnail,
-      multiple_images: storeImages,
     }));
-  }, [roomInfos, usageFees, cancelTime, thumbnail, storeImages]);
+  }, [roomInfos, usageFees, cancelTime]);
 
   return {
     roomInfos,
     cancelTime,
     usageFees,
     formData,
-    thumbnail,
-    storeImages,
     setUsageFees,
     setRoomInfos,
     setFormData,
     setCancelTime,
     handleInputChange,
-    handleNestedInputChange,
     handleSelectChange,
-    handleRoomChange,
     handleRoomNameChange,
-    handleAddRoom,
-    handleRemoveRoom,
-    handleAddFee,
-    handleRemoveFee,
-    handleFeeChange,
     handleThumbnailChange,
     handleStoreImagesChange,
+
+    handleRemoveItem,
+    handleAddItem,
+    handleArrayChange,
   };
 };
 
