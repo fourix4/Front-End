b11839efa0193d881519761945d8540838537ed9
ejinn1 <ejinn1@naver.com>
2024-07-28 14:26:18 +0900

    [#97] fix : file 전송을 위해 FormData 객체로 변경

--- catch-study/src/apis/api/manager.ts
+++ catch-study/src/apis/api/manager.ts
@@ -1,14 +1,30 @@
 import { API_ADDRESS } from '../../config/api';
 import { ErrorResponseTypes, ResponseTypes } from '../../types/interfaces';
-
 import { FormDataTypes } from '../../types/management';
 import instance from '../utils/axios';
 
+export interface DataTypes {
+  [key: string]: any;
+}
+
+const createFormData = (data: DataTypes): FormData => {
+  const formData = new FormData();
+
+  for (const key in data) {
+    if (key in data && data[key] !== null) {
+      formData.append(key, data[key]);
+    }
+  }
+  return formData;
+};
+
 export const postManagementInfo = async (formData: FormDataTypes) => {
   try {
+    const dataForm = createFormData(formData);
+
     const { data } = await instance.post<ResponseTypes>(
       API_ADDRESS.MANAGER_INFO_URI,
-      formData,
+      dataForm,
       {
         headers: {
           'Content-Type': 'multipart/form-data',
@@ -26,9 +42,11 @@ export const postManagementInfo = async (formData: FormDataTypes) => {
 
 export const patchManagementInfo = async (editFormData: FormDataTypes) => {
   try {
+    const dataForm = createFormData(editFormData);
+
     const { data } = await instance.patch<ResponseTypes>(
       API_ADDRESS.MANAGER_INFO_URI,
-      editFormData,
+      dataForm,
       {
         headers: {
           'Content-Type': 'multipart/form-data',
