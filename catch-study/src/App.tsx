add5ec2b03447fb849dc94a7662e60e581dbf43b
조현정 <79002373+HyunJungJo98@users.noreply.github.com>
2024-08-02 14:51:47 +0900

    [#106] refactor : route 페이지 lazy, suspense 적용

--- catch-study/src/App.tsx
+++ catch-study/src/App.tsx
@@ -1,45 +1,74 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { ROUTE } from './config/constants';
-import BookingPage from './pages/BookingPage/BookingPage';
-import ChattingPage from './pages/ChattingPage/ChattingPage';
-import ChattingRoomPage from './pages/ChattingRoomPage/ChattingRoomPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import MainPage from './pages/MainPage/MainPage';
-import ManagementEditPage from './pages/ManagementEditPage/ManagementEditPage';
-import ManagementFormPage from './pages/ManagementFormPage/ManagementFormPage';
-import ManagementPage from './pages/ManagementPage/ManagementPage';
-import MyPage from './pages/MyPage/MyPage';
-import PaymentPage from './pages/PaymentPage/PaymentPage';
-import PaymentSuccessPage from './pages/PaymentSuccessPage/PaymentSuccessPage';
-import RedirectionPage from './pages/RedirectionPage/RedirectionPage';
-import StudycafeBookingPage from './pages/StudycafeBookingPage/StudycafeBookingPage';
-import GoogleRedirectionPage from './pages/GoogleRedirectionPage/GoogleRedirectionPage';
+
+const MainPage = lazy(() => import('./pages/MainPage/MainPage'));
+const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const RedirectionPage = lazy(
+  () => import('./pages/RedirectionPage/RedirectionPage'),
+);
+const ChattingPage = lazy(() => import('./pages/ChattingPage/ChattingPage'));
+const ChattingRoomPage = lazy(
+  () => import('./pages/ChattingRoomPage/ChattingRoomPage'),
+);
+const MyPage = lazy(() => import('./pages/MyPage/MyPage'));
+const ManagementPage = lazy(
+  () => import('./pages/ManagementPage/ManagementPage'),
+);
+const ManagementFormPage = lazy(
+  () => import('./pages/ManagementFormPage/ManagementFormPage'),
+);
+const ManagementEditPage = lazy(
+  () => import('./pages/ManagementEditPage/ManagementEditPage'),
+);
+const StudycafeBookingPage = lazy(
+  () => import('./pages/StudycafeBookingPage/StudycafeBookingPage'),
+);
+const PaymentPage = lazy(() => import('./pages/PaymentPage/PaymentPage'));
+const PaymentSuccessPage = lazy(
+  () => import('./pages/PaymentSuccessPage/PaymentSuccessPage'),
+);
+const BookingPage = lazy(() => import('./pages/BookingPage/BookingPage'));
+const GoogleRedirectionPage = lazy(
+  () => import('./pages/GoogleRedirectionPage/GoogleRedirectionPage'),
+);
 
 const App: React.FC = () => {
   return (
     <>
-      <Routes>
-        <Route path={ROUTE.HOME} element={<MainPage />} />
-        <Route path={ROUTE.LOGIN} element={<LoginPage />} />
-        <Route path={ROUTE.KAKAO_REDIRECT} element={<RedirectionPage />} />
-        <Route path={ROUTE.CHATTING} element={<ChattingPage />} />
-        <Route path={ROUTE.CHATTING_ROOM} element={<ChattingRoomPage />} />
-        <Route path={ROUTE.MYPAGE} element={<MyPage />} />
-        <Route path={ROUTE.MANAGEMENT} element={<ManagementPage />} />
-        <Route path={ROUTE.MANAGEMENT_FORM} element={<ManagementFormPage />} />
-        <Route path={ROUTE.MANAGEMENT_EDIT} element={<ManagementEditPage />} />
-        <Route
-          path={ROUTE.STUDYCAFE_BOOKING}
-          element={<StudycafeBookingPage />}
-        />
-        <Route path={ROUTE.PAYMENT} element={<PaymentPage />} />
-        <Route path={ROUTE.PAYMENT_SUCCESS} element={<PaymentSuccessPage />} />
-        <Route path={ROUTE.BOOKING} element={<BookingPage />} />
-        <Route
-          path={ROUTE.GOOGLE_REDIRECT}
-          element={<GoogleRedirectionPage />}
-        />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={ROUTE.HOME} element={<MainPage />} />
+          <Route path={ROUTE.LOGIN} element={<LoginPage />} />
+          <Route path={ROUTE.KAKAO_REDIRECT} element={<RedirectionPage />} />
+          <Route path={ROUTE.CHATTING} element={<ChattingPage />} />
+          <Route path={ROUTE.CHATTING_ROOM} element={<ChattingRoomPage />} />
+          <Route path={ROUTE.MYPAGE} element={<MyPage />} />
+          <Route path={ROUTE.MANAGEMENT} element={<ManagementPage />} />
+          <Route
+            path={ROUTE.MANAGEMENT_FORM}
+            element={<ManagementFormPage />}
+          />
+          <Route
+            path={ROUTE.MANAGEMENT_EDIT}
+            element={<ManagementEditPage />}
+          />
+          <Route
+            path={ROUTE.STUDYCAFE_BOOKING}
+            element={<StudycafeBookingPage />}
+          />
+          <Route path={ROUTE.PAYMENT} element={<PaymentPage />} />
+          <Route
+            path={ROUTE.PAYMENT_SUCCESS}
+            element={<PaymentSuccessPage />}
+          />
+          <Route path={ROUTE.BOOKING} element={<BookingPage />} />
+          <Route
+            path={ROUTE.GOOGLE_REDIRECT}
+            element={<GoogleRedirectionPage />}
+          />
+        </Routes>
+      </Suspense>
     </>
   );
 };
