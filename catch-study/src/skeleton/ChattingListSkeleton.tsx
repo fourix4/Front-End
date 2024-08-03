892669ed1d6efe81feef4db8a912ed13c93c0adf
ejinn1 <ejinn1@naver.com>
2024-08-03 12:20:22 +0900

    [#71] style : chattingList 스켈레톤 컴포넌트 ui

---
+++ catch-study/src/skeleton/ChattingListSkeleton.tsx
@@ -0,0 +1,13 @@
+import Skeleton from './Skeleton';
+
+const ChattingListSkeleton = () => {
+  return (
+    <div className='relative p-20 h-100 [&>*]:mb-10'>
+      <Skeleton width='w-70' />
+      <Skeleton width='w-140' />
+      <Skeleton width='w-40' className='absolute top-40 right-20' />
+    </div>
+  );
+};
+
+export default ChattingListSkeleton;
