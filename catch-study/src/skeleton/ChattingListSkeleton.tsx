684a20d85757475f6740c8e440dd1dd270d4e5cb
ejinn1 <ejinn1@naver.com>
2024-08-03 13:44:06 +0900

    [#73] chore : 채팅 스켈레톤 5개로 변경

--- catch-study/src/skeleton/ChattingListSkeleton.tsx
+++ catch-study/src/skeleton/ChattingListSkeleton.tsx
@@ -2,11 +2,20 @@ import Skeleton from './Skeleton';
 
 const ChattingListSkeleton = () => {
   return (
-    <div className='relative p-20 h-100 [&>*]:mb-10 border-b-2 border-light-gray'>
-      <Skeleton width='w-70' />
-      <Skeleton width='w-140' />
-      <Skeleton width='w-40' className='absolute top-40 right-20' />
-    </div>
+    <>
+      {Array(5)
+        .fill(0)
+        .map((_, i) => (
+          <div
+            key={i}
+            className='relative p-20 h-100 [&>*]:mb-10 border-b-2 border-bright-gray'
+          >
+            <Skeleton width='w-70' />
+            <Skeleton width='w-140' />
+            <Skeleton width='w-40' className='absolute top-40 right-20' />
+          </div>
+        ))}
+    </>
   );
 };
 
