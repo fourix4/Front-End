4f81d391797cf58311e57f07dff56eaee7d0abe6
ejinn1 <ejinn1@naver.com>
2024-08-03 13:02:50 +0900

    [#73] feat : chattingList 스켈레톤 적용

--- catch-study/src/pages/ChattingPage/ChattingPage.tsx
+++ catch-study/src/pages/ChattingPage/ChattingPage.tsx
@@ -7,12 +7,14 @@ import { isAuthUser } from '../../apis/services/user';
 import ChattingRoomList from '../../components/ChattingRoomList/ChattingRoomList';
 import Topbar from '../../components/Topbar/Topbar';
 import { ROUTE } from '../../config/constants';
+import ChattingListSkeleton from '../../skeleton/ChattingListSkeleton';
 import { ChattingRoomTypes } from '../../types/chatting';
 
 const ChattingPage: React.FC = () => {
   const navigate = useNavigate();
   const [authChecked, setAuthChecked] = useState(false);
   const [chattingRooms, setChattingRooms] = useState<ChattingRoomTypes[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
@@ -31,13 +33,18 @@ const ChattingPage: React.FC = () => {
       const roomData = getChattingRoomData(roomRawData);
 
       setChattingRooms(roomData);
+      setIsLoading(false);
     })();
   }, [navigate]);
 
   return (
     <>
       <Topbar />
-      {authChecked && <ChattingRoomList rooms={chattingRooms} />}
+      {isLoading ? (
+        <ChattingListSkeleton />
+      ) : (
+        <>{authChecked && <ChattingRoomList rooms={chattingRooms} />}</>
+      )}
     </>
   );
 };
--- catch-study/src/skeleton/ChattingListSkeleton.tsx
+++ catch-study/src/skeleton/ChattingListSkeleton.tsx
@@ -2,7 +2,7 @@ import Skeleton from './Skeleton';
 
 const ChattingListSkeleton = () => {
   return (
-    <div className='relative p-20 h-100 [&>*]:mb-10'>
+    <div className='relative p-20 h-100 [&>*]:mb-10 border-b-2 border-light-gray'>
       <Skeleton width='w-70' />
       <Skeleton width='w-140' />
       <Skeleton width='w-40' className='absolute top-40 right-20' />
