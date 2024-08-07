2e572c73afb91d4be537dc5b07bafbaaa96c5880
ejinn1 <ejinn1@naver.com>
2024-08-07 11:48:47 +0900

    [#73] refactor : 상세정보 스켈레톤 적용

--- catch-study/src/pages/ManagementDetailPage/ManagementDetailPage.tsx
+++ catch-study/src/pages/ManagementDetailPage/ManagementDetailPage.tsx
@@ -1,10 +1,14 @@
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getManagementInfo } from '../../apis/api/manager';
 import { getCafeInfoData } from '../../apis/services/manager';
-import ManagementCafeInfo from '../../components/ManagementCafeInfo/ManagementCafeInfo';
 import Topbar from '../../components/Topbar/Topbar';
+import ManagementCafeInfoSkeleton from '../../skeletons/ManagementCafeInfoSkeleton';
 import { CafeInfoTypes } from '../../types/management';
+
+const ManagementCafeInfo = lazy(
+  () => import('../../components/ManagementCafeInfo/ManagementCafeInfo'),
+);
 
 const ManagementDetailPage: React.FC = () => {
   const { cafeId } = useParams<{ cafeId: string }>();
@@ -27,7 +31,9 @@ const ManagementDetailPage: React.FC = () => {
     <>
       <Topbar />
       <div className='flex flex-col items-center w-full h-full gap-20 p-20'>
-        {cafeInfo && <ManagementCafeInfo cafeInfo={cafeInfo} />}
+        <Suspense fallback={<ManagementCafeInfoSkeleton />}>
+          {cafeInfo && <ManagementCafeInfo cafeInfo={cafeInfo} />}
+        </Suspense>
       </div>
     </>
   );
