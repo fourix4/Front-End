321018a5405c1cceff190113f71d0bb8c12edbc3
ejinn1 <ejinn1@naver.com>
2024-08-07 11:46:32 +0900

    [#73] refactor : 카페 현황 스켈레톤 적용

--- catch-study/src/pages/ManagementPage/ManagementPage.tsx
+++ catch-study/src/pages/ManagementPage/ManagementPage.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react';
+import { lazy, Suspense, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getCafeStatus } from '../../apis/api/manager';
 import { getCafeStatusData } from '../../apis/services/manager';
-import ManagementCafeStatus from '../../components/ManagementCafeStatus/ManagementCafeStatus';
 import Topbar from '../../components/Topbar/Topbar';
 import { ROUTE } from '../../config/constants';
 import useAuthCheck from '../../hooks/useAuthCheck';
+import ManagementCafeStatusSkeleton from '../../skeletons/ManagementCafeStatusSkeleton';
 import { CafeStatusTypes } from '../../types/management';
+
+const ManagementCafeStatus = lazy(
+  () => import('../../components/ManagementCafeStatus/ManagementCafeStatus'),
+);
 
 const ManagementPage: React.FC = () => {
   useAuthCheck();
@@ -33,9 +37,11 @@ const ManagementPage: React.FC = () => {
     <>
       <Topbar />
       <div className='flex flex-col items-center w-full h-full gap-20 p-20'>
-        {cafeStatus.map(cafe => (
-          <ManagementCafeStatus key={cafe.cafe_id} cafeStatus={cafe} />
-        ))}
+        <Suspense fallback={<ManagementCafeStatusSkeleton />}>
+          {cafeStatus.map(cafe => (
+            <ManagementCafeStatus key={cafe.cafe_id} cafeStatus={cafe} />
+          ))}
+        </Suspense>
         <div className='w-full sm:w-smWeb lg:w-lgWeb'>
           <Link to={ROUTE.MANAGEMENT_FORM}>
             <button className='input-box'>스터디 카페 정보 입력</button>
